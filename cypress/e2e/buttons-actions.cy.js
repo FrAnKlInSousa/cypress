bb5aff2f3d8008da11bf4645856730113436b259
franklin <frank.fkn.23@gmail.com>
2026-01-07 18:57:46 -0300

    testando caso de mudança do botão desabilitado

--- cypress/e2e/buttons-actions.cy.js
+++ cypress/e2e/buttons-actions.cy.js
@@ -10,6 +10,7 @@ function changeButtonText() {
     .as('confirm-button')
     .should('have.text', 'Confirmar')
     .click();
+  cy.wait(50);
   cy.get('@confirm-button').should('have.text', 'Parabéns, gênio!');
 }
 
@@ -23,20 +24,34 @@ function changeButtonColor() {
     .should('have.text', 'Mudou de cor');
   cy.get('@confirm-button').should('have.css', 'background-color');
 }
+
+function changeDisableButtonToEnable() {
+  cy.wait(50);
+  cy.get('[data-test="button"]')
+    .as('button')
+    .should('have.text', 'Clique aqui');
+  cy.get('@button').should('be.disabled');
+
+  cy.get('[data-test="checkbox"]').check();
+  cy.get('@button').should('be.enabled').click().should('have.text', 'Clicou!');
+
+  cy.get('[data-test="checkbox"]').uncheck();
+  cy.get('@button').should('be.disabled').should('have.text', 'Clique aqui');
+}
+
 describe('check buttons behaviors on dummy page', () => {
-  beforeEach(() => {
-    cy.visit('/');
-    cy.wait(50);
+  it('check if button changes the text value', () => {
+    cy.visit('buttons');
+    changeButtonText();
   });
 
-  it('check if button Botões redirects to /buttons', () => {
-    buttonRedirect();
-
-    changeButtonText();
-
-    cy.get('[data-test="next-button"]').click();
-    cy.url().should('include', 'cypress/buttons/case-2');
-
+  it('check if color button changes', () => {
+    cy.visit('buttons/case-2');
     changeButtonColor();
+  });
+
+  it.only('check the state changes of disabled button', () => {
+    cy.visit('buttons/case-3');
+    changeDisableButtonToEnable();
   });
 });
