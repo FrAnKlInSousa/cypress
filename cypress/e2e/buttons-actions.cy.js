7b24626d93bdccd9a248d9539f1a90f61c1cdf1c
franklin <frank.fkn.23@gmail.com>
2026-01-07 11:38:05 -0300

    organizando testes e adicionando wait para renderizar as telas durante os testes

--- cypress/e2e/buttons-actions.cy.js
+++ cypress/e2e/buttons-actions.cy.js
@@ -1,39 +1,42 @@
 function buttonRedirect() {
   cy.get('[data-test="button-option"]').as('button');
-  cy.get('@button').should('have.text', 'Botões');
-  cy.get('@button').click();
+  cy.get('@button').should('have.text', 'Botões').click();
+  cy.url().should('include', '/cypress/buttons');
 }
 
 function changeButtonText() {
+  cy.wait(50);
   cy.get('[data-test="confirm-button"]')
     .as('confirm-button')
     .should('have.text', 'Confirmar')
     .click();
+  cy.get('@confirm-button').should('have.text', 'Parabéns, gênio!');
 }
 
 function changeButtonColor() {
+  cy.wait(50);
+
   cy.get('[data-test="confirm-button"]')
     .as('confirm-button')
     .should('have.text', 'Clique aqui')
     .click()
     .should('have.text', 'Mudou de cor');
+  cy.get('@confirm-button').should('have.css', 'background-color');
 }
 describe('check buttons behaviors on dummy page', () => {
   beforeEach(() => {
     cy.visit('/');
+    cy.wait(50);
   });
 
   it('check if button Botões redirects to /buttons', () => {
     buttonRedirect();
-    cy.url().should('include', '/cypress/buttons');
 
     changeButtonText();
-    cy.get('@confirm-button').should('have.text', 'Parabéns, gênio!');
 
     cy.get('[data-test="next-button"]').click();
     cy.url().should('include', 'cypress/buttons/case-2');
 
     changeButtonColor();
-    cy.get('@confirm-button').should('have.css', 'background-color');
   });
 });
